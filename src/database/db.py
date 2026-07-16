46c4c86db02a4759c574a5dd3319ce106e6d161d
Nishant Shrestha <shresthanis2255@gmail.com>
2026-07-16 19:21:05 +0545

    feat: implement teacher registration and login functionality with database integration

---
+++ src/database/db.py
@@ -0,0 +1,59 @@
+from src.database.config import supabase
+import bcrypt
+
+# Password Hashing
+def hash_pass(password):
+    return bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()
+
+# Password Validation
+def validate_password(pwd, hashed):
+    return bcrypt.checkpw(pwd.encode(), hashed.code())
+
+def check_teacher_exits(username):
+    # check the unique username, return false when username already taken 
+    response = supabase.table("teachers").select("username").eq("username", username).execute()
+    return len(response.data) > 0
+
+
+def create_teacher(username, password, name):
+    data = {
+        "username": username,
+        "password": hash_pass(password),
+        "name": name
+        }
+    response = supabase.table("teachers").insert(data).execute()
+    return response.data
+
+def login_teacher(username, password):
+    response = supabase.table("teachers").select("*").eq("username", username).execute()
+    if response.data:
+        teacher = response.data[0]
+        if validate_password(password, teacher['password']):
+            return teacher
+        return None
+    
+def get_all_students():
+    response = supabase.table("students").select("*").execute()
+    return response.data
+
+def create_student(name, face_embedding=None, voice_embedding=None):
+    data = {
+        "name": name,
+        "face_embedding": face_embedding,
+        "voice_embedding": voice_embedding
+    }
+    response = supabase.table("students").insert(data).execute()
+    return response.data
+
+def create_subject(subject_code, name, section, teacher_id):
+    data = {
+        "subject_code": subject_code, 
+        "name": name, "section": section, 
+        "teacher_id": teacher_id
+        }
+    response = supabase.table("subjects").insert(data).execute()
+    return response.data
+
+def get_teacher_subject(teacher_id):
+    response = supabase.table("subjects").select("*, subject_students(count), attendence_logs(timestamp)").eq("teacher_id", teacher_id).execute()   
+    subjects = response.data
--- src/screens/teacher_screen.py
+++ src/screens/teacher_screen.py
@@ -2,20 +2,63 @@ import streamlit as st
 from src.ui.base_layout import style_background_dashboard, style_base_layout
 from src.components.header import header_dashboard
 from src.components.footer import footer
+from src.database.db import check_teacher_exits, create_teacher, login_teacher
 
 def teacher_screen():
-
     header_dashboard()
     style_background_dashboard()
     style_base_layout()
 
-    teacher_screen_login()
+    if 'teacher_login_type' not in st.session_state or st.session_state.teacher_login_type == "login":
+        teacher_screen_login()
+    elif st.session_state.teacher_login_type == "register":
+        teacher_screen_register()
 
     footer()
 
+def register_teacher(teacher_username, teacher_name, teacher_pass, teacher_pass_confirm):
+    if not teacher_name or not teacher_username or not teacher_pass:
+        return False, "All fields are required!"
+    if check_teacher_exits(teacher_username):
+        return False, "Username already taken."
+    if teacher_pass != teacher_pass_confirm:
+        return False, "Password doesn't match."
+    try:
+        create_teacher(teacher_username, teacher_pass, teacher_name)
+        return True, "Successfully Created! Login Now"
+    except Exception as e:
+        return False, "Unexpected Error!"
 
 def teacher_screen_register():
-    st.header("Register your teacher profile", text_alignment="center")
+    with st.container(key="portal_cards"):
+        st.subheader("Register your teacher profile", text_alignment="center")
+        st.space()
+        teacher_username = st.text_input("Enter username", placeholder='@username')
+
+        teacher_name = st.text_input("Enter name", placeholder='Ananya Roy')
+
+        teacher_pass = st.text_input("Enter password", type='password', placeholder="Enter password")
+
+        teacher_pass_confirm = st.text_input("Confirm your password", type='password', placeholder="Enter password")
+        st.divider()
+
+        btnc1, btnc2 = st.columns(2, gap="small")
+        with btnc1:
+            if st.button('Register Now', icon=":material/passkey:", width="stretch"):
+                success, message = register_teacher(teacher_username, teacher_name, teacher_pass, teacher_pass_confirm)
+                if success:
+                    st.success(message)
+                    import time
+                    time.sleep(2)
+                    st.session_state['teacher_login_type'] = "login"
+                    st.rerun()
+                else:
+                    st.error(message)
+        with btnc2:
+            if st.button('Login Instead', type="tertiary", icon=":material/passkey:", width="stretch"):
+                st.session_state['teacher_login_type'] = 'login'
+                st.rerun()
+
     
 
 def teacher_screen_login():
@@ -25,10 +68,20 @@ def teacher_screen_login():
         teacher_username = st.text_input("Username:", placeholder="@username")
         teacher_password = st.text_input("Password:", type="password", placeholder="Enter password")
 
-        st.space()
+        st.divider()
         btnc1, btnc2 = st.columns(2, gap="small")
         with btnc1:
-            st.button('Login', icon=":material/passkey:", width="stretch")
+            if st.button('Login', icon=":material/passkey:", width="stretch"):
+                if login_teacher(teacher_username, teacher_password):
+                    st.toast("welcome back!", icon="👋")
+                    import time
+                    time.sleep(1)
+                    st.rerun()
+                else:
+                    st.error("Invalid username and password")
+        
         with btnc2:
-            st.button('Register Instead', type="tertiary", icon=":material/passkey:", width="stretch")
+            if st.button('Register Instead', type="tertiary", icon=":material/passkey:", width="stretch"):
+                st.session_state['teacher_login_type'] = 'register'
+                st.rerun()
 
